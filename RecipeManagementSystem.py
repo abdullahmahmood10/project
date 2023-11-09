8ef03f4aa7a8d89939fe22596f05cb8971b2cf39
Abdullah <60100820@udst.edu.qa>
2023-11-09 08:54:03 +0300

    Add Class Recipe

---
+++ RecipeManagementSystem.py
@@ -0,0 +1,10 @@
+import random 
+
+class Recipe:
+    def __init__ (self,recipe_id, name, ingredients, instructions, category, rating):
+        self.id = recipe_id
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.category = category
+        self.rating = rating
