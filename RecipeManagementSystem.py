9c926d9524ad036cfb7bb3b46d0e28dc7fbcf8a6
Hamaad <60098334@udst.edu.qa>
2023-11-16 08:59:43 +0300

    adding function view recipes

--- RecipeManagementSystem.py
+++ RecipeManagementSystem.py
@@ -51,4 +51,31 @@ class RecipeManagementSystem:
                 print("Invalid rating. Please enter a number between 1 and 5.")
         except ValueError:
             print("Invalid input. Rating must be a number between 1 and 5.")  
+            
+            
+    def view_recipes(self):
+        if not self.recipes:
+            print("No recipes found.")
+        else:
+            print("View Recipes\n")
+            print("1. Display all recipes")
+            print("2. Search by keyword")
+            print("3. Filter by category")
+            print("4. Filter by rating")
+
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                self.display_all_recipes()
+            elif choice == '2':
+                keyword = input("Enter a keyword to search for: ")
+                self.search_by_keyword(keyword)
+            elif choice == '3':
+                category = input("Enter a category to filter by: ")
+                self.filter_by_category(category)
+            elif choice == '4':
+                rating = input("Enter a rating to filter by (1-5): ")
+                self.filter_by_rating(rating)
+            else:
+                print("Invalid choice.")
     
